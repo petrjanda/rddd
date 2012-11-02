d7c5f8c850918e6f351a71e25b513814fbb9041d
Petr Janda <petrjanda@me.com>
2012-11-03 00:49:06 +0100

    explain service

--- lib/rddd/service.rb
+++ lib/rddd/service.rb
@@ -1,3 +1,12 @@
+#
+# Service is the object to represent a single use case. Service is usually good
+# entry point to the system domain. Its responsibility is to orchestrate the
+# cooperation of multiple entities, in a given order defined by use case.
+#
+# Service takes a list of attributes, which are necessary to execute the given
+# use case. At the end nothing is returned, so service represent the command to
+# the domain, but doesn't return any data back.
+#
 class Service
   def initialize(attributes)
     @attributes = attributes
