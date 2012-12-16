b7f03cef37ac054835e0ba7a9ce3c8b37df041eb
Petr Janda <petrjanda@me.com>
2012-12-16 12:06:27 +0100

    refactor finders

--- lib/rddd/aggregate_root_finders.rb
+++ lib/rddd/aggregate_root_finders.rb
@@ -1,15 +1,21 @@
 module Rddd
   module AggregateRootFinders
-    def finder(name, &block)
-      (class << self; self; end).instance_eval do
-        define_method name do |*args|
-          repository.send(name, *args)
-        end
+    def finder(name)
+      craete_static_method(name) do |*args|
+        repository.send(name, *args)
       end
     end
 
     def repository
       RepositoryFactory.build(self)
+    end
+
+    private
+
+    def craete_static_method(name, &block)
+      (class << self; self; end).instance_eval do
+        define_method name, &block
+      end
     end
   end
 end
