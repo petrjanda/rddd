e6caad48584b9219ed9718483f861bdf756bb407
Petr Janda <petrjanda@me.com>
2012-12-26 11:26:20 +0100

    adding more docs

--- lib/rddd/aggregates/aggregate_root.rb
+++ lib/rddd/aggregates/aggregate_root.rb
@@ -4,6 +4,12 @@ require 'rddd/aggregates/finders'
 
 module Rddd
   module Aggregates
+    #
+    # Domain model Entities (see Entity#initialize) are organized to clusters
+    # called Aggregates. Every Aggregate should have its Root. Aggregate Root
+    # should be only entity from Aggregate visible from outside to guarantee
+    # consistency of all operations performed on the given Aggregate.
+    #
     class AggregateRoot < Entity
       extend Finders
 
